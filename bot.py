a275ca8d2d352c1d231b0f88e26bec849f274a7c
irutheu <irutheu@yandex.ru>
2018-12-15 22:53:05 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 token = os.environ['TELEGRAM_TOKEN']
 
@@ -15,4 +15,11 @@ def startCommand(bot, update):
 start_handler = CommandHandler('start', startCommand)
 dispatcher.add_handler(start_handler)
 
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
 updater.start_polling()
+
