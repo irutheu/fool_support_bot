24b45067ff4d713e9275ccd656dbb47ce614a59f
irutheu <irutheu@yandex.ru>
2018-12-15 22:44:38 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,23 +1,18 @@
 import telegram
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-import logging
+from telegram.ext import Updater, CommandHandler
 
 token = os.environ['TELEGRAM_TOKEN']
 
 #bot = telegram.Bot(token=token)
 
 updater = Updater(token=token)
-
 dispatcher = updater.dispatcher
 
-#logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Not dead yet")
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Not dead yet')
 
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('start', startCommand)
 dispatcher.add_handler(start_handler)
-
 
 updater.start_polling()
