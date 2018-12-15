a375d42aa40354b541f652ebcd8afe1b524d9ed5
irutheu <irutheu@yandex.ru>
2018-12-15 22:36:26 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,19 +5,16 @@ import logging
 
 token = os.environ['TELEGRAM_TOKEN']
 
-bot = telegram.Bot(token=token)
+#bot = telegram.Bot(token=token)
 
 updater = Updater(token=token)
 
 dispatcher = updater.dispatcher
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+#logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Not dead yet")
-    
-
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
